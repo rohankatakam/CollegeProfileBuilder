ceb03f5535f7ddaf893ce543ac7cd907eebe4ad0
Matthew Wen <042685m@gmail.com>
2016-01-21 13:56:31 -0600

    Allow for entries to be re-ordered

--- College Hackwich/ViewController.swift
+++ College Hackwich/ViewController.swift
@@ -11,44 +11,57 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
+    @IBOutlet weak var editButton: UIBarButtonItem!
     @IBOutlet weak var tableView: UITableView!
     
     var colleges = ["Yale", "Harvard", "UIC"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        editButton.tag = 0
     
     }
-    
+    //count # of table views to have
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return colleges.count
     }
-    
+    //cell display city
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
         cell.textLabel?.text = colleges[indexPath.row]
         return cell
-        
-    }
     
+    }
+    //allow table view to be editied
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    //allows the table view to be moved around 
+    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        let college = colleges[sourceIndexPath.row]
+        colleges.removeAtIndex(sourceIndexPath.row)
+        colleges.insert(college, atIndex: destinationIndexPath.row)
+    }
+    //
+    //Delete a cell
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             colleges.removeAtIndex(indexPath.row)
             tableView.reloadData()
         }
+        
     }
-    
+    //Tap the plus button
     @IBAction func onTappedPlusButton(sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
-       
+        //textfield
         alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
             textField.placeholder = "Add College Here"
         }
-        
+        //cancel Button
         let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
         alert.addAction(cancelAction)
-        
+        //Add Button
         let addAction = UIAlertAction(title: "Add", style: .Default) { (action) -> Void in
             let collegeTextField = alert.textFields![0] as UITextField
             self.colleges.append(collegeTextField.text!)
@@ -57,8 +70,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         alert.addAction(addAction)
         self.presentViewController(alert, animated: true, completion: nil)
+    }
+    //Tap the Edit Button
+    @IBAction func onTappedEditButton(sender: UIBarButtonItem) {
+        if sender.tag == 0{
+            tableView.editing = true
+            sender.tag = 1
+        }
+        else {
+            tableView.editing = false
+            sender.tag = 0
+        }
     
     }
-
+    
+    
 }
 
