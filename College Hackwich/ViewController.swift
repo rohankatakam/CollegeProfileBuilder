5d19212ec55345521a3712ac80017fa1814858e6
Matthew Wen <042685m@gmail.com>
2016-01-21 12:32:40 -0600

    Allow for entries to be added

--- College Hackwich/ViewController.swift
+++ College Hackwich/ViewController.swift
@@ -39,6 +39,26 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
+    @IBAction func onTappedPlusButton(sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
+       
+        alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            textField.placeholder = "Add College Here"
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (action) -> Void in
+            let collegeTextField = alert.textFields![0] as UITextField
+            self.colleges.append(collegeTextField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addAction(addAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    
+    }
 
 }
 
