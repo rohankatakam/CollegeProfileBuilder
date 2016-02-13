41d7cf3746ca430dbd67ee4530796d03b4bd515e
Matthew Wen <042685m@gmail.com>
2016-02-12 19:42:54 -0600

    added a map to this app

--- College Hackwich/DetailMapViewController.swift
+++ College Hackwich/DetailMapViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import MapKit
 
-class DetailMapViewController: UIViewController {
+class DetailMapViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var locatonTextField: UITextField!
+    
+    var college = Colleges()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locatonTextField.delegate = self
+        findLocation(college.location)
         
         
     }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        let locationName = locatonTextField.text!
+        textField.resignFirstResponder()
+        findLocation(locationName)
+        return true
+    }
+    
+    func displayMap(placemark: CLPlacemark){
+        locatonTextField.text = placemark.name
+        let center = placemark.location!.coordinate
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(center, span)
+        let pin = MKPointAnnotation()
+        pin.coordinate = center
+        pin.title = placemark.name
+        mapView.addAnnotation(pin)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func findLocation(locationName: String){
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(locationName) { (placemarks, error) -> Void in
+            if error != nil{
+                print(error)
+            }
+            else{
+                if placemarks!.count > 1{
+                    let alert = UIAlertController(title: "Select a Lcoation", message: nil, preferredStyle: .ActionSheet)
+                    for placemark in placemarks!{
+                        let locationAction = UIAlertAction(title: placemark.name, style: .Default, handler: { (action) -> Void in
+                            self.displayMap(placemark)
+                        })
+                        alert.addAction(locationAction)
+                    }
+                    let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+                    alert.addAction(cancelAction)
+                    self.presentViewController(alert, animated: true, completion: nil)
+                }
+                
+                else if placemarks!.count == 1{
+                    let placemark = placemarks!.first as! CLPlacemark!
+                    self.displayMap(placemark)
+                }
+                
+                let placemark = placemarks!.first! as CLPlacemark
+                self.displayMap(placemark)
+            }
+            
+        }
+    }
+    
 
     @IBAction func doneButtonOnTap(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
